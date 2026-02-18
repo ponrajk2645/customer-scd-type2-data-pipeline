aaf49cd3e07e066b9d5cc4507266e1e9ac58384f
ponrajk2645 <raj8870975892k@gmail.com>
2026-02-18 17:40:37 +0530

    Update create_staging_customer.sql

--- create_staging_customer.sql
+++ create_staging_customer.sql
@@ -1,1 +1,18 @@
-r
+CREATE TABLE IF NOT EXISTS stg_customer (
+    customer_id INT NOT NULL,
+
+    first_name VARCHAR(100),
+    last_name VARCHAR(100),
+    email VARCHAR(150),
+    phone_number VARCHAR(20),
+    email_verified TINYINT(1),
+
+    city VARCHAR(50),
+    plan VARCHAR(50),
+    subscription_status VARCHAR(50),
+    billing_cycle VARCHAR(50),
+    event_date DATE NOT NULL,
+    source_system VARCHAR(50),
+
+    PRIMARY KEY (customer_id, event_date)
+);
